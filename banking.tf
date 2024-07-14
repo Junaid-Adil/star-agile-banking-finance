39bad36b36c7dd36915c9c187a254a0ad2de0bd5
Junaid-Adil <167700525+Junaid-Adil@users.noreply.github.com>
2024-07-14 17:49:51 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -125,11 +125,11 @@ resource "aws_eip" "proj-eip" {
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
+resource "aws_instance" "Test-Server" {
+ ami = "ami-0c2af51e265bd5e0e"
  instance_type = "t2.micro"
- availability_zone = "ap-south-1b"
- key_name = "shiva-mentor"
+ availability_zone = "ap-south-1"
+ key_name = "Demo1keypair"
  network_interface {
  device_index = 0
  network_interface_id = aws_network_interface.proj-ni.id
